e6859c8f1bae740a4e0f5409d857d0ac10b09aac
Sehny <sehnoutka.martin@gmail.com>
2015-09-18 22:02:22 +0300

    Multi-thread switch prototype

---
+++ switch.hs
@@ -0,0 +1,51 @@
+import Network
+import System.IO (hGetLine, hPutStrLn, hPutStr)
+import Control.Concurrent (forkIO)
+import Control.Monad.STM (atomically)
+import Control.Concurrent.STM.TChan (TChan, newTChan, dupTChan, readTChan, tryReadTChan, writeTChan)
+
+main :: IO()
+main = do
+  controlChannel <- atomically newTChan
+  readChannel <- atomically newTChan
+  let channels = (controlChannel, readChannel)
+  socket <- listenOn $ PortNumber 4242
+  forkIO $ switch channels []
+  acceptLoop socket channels 0
+
+acceptLoop socket channels address = do
+  (handle,_,_) <- accept socket
+  let controlChannel = fst channels
+      readChannel    = snd channels
+  clientReadChannel <- atomically $ dupTChan readChannel
+  clientWriteChannel <- atomically newTChan
+  forkIO $ readFromClient handle clientReadChannel
+  forkIO $ writeToClient handle clientWriteChannel
+  atomically $ writeTChan controlChannel (address,clientWriteChannel)
+  acceptLoop socket channels (address+1)
+
+switch channels clients = do
+  let controlChannel = fst channels
+      readChannel    = snd channels
+  message <- atomically $ readTChan readChannel
+  newClients <- importNewClients controlChannel clients
+  mapM (\(_,channel) -> atomically (writeTChan channel message)) newClients
+  switch channels newClients
+
+importNewClients controlChannel clients = do
+  maybeClient <- atomically $ tryReadTChan controlChannel
+  case maybeClient of
+    Nothing -> return clients
+    Just clientTuple -> importNewClients controlChannel (clientTuple:clients)
+
+readFromClient handle channel = do
+  hPutStr handle "Prompt> "
+  message <- hGetLine handle
+  putStrLn $ "[client channel] " ++ message
+  atomically $ writeTChan channel message
+  readFromClient handle channel
+
+writeToClient handle channel = do
+  message <- atomically $ readTChan channel
+  hPutStrLn handle message
+  writeToClient handle channel
